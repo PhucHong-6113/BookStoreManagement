ed9ca26cb7121476e8b4bbbb838023f9ac6bfa62
PhucHong-6113 <phuongdubu@gmail.com>
2024-07-03 15:57:05 +0700

    update Model

--- BusinessObject/Models/Publisher.cs
+++ BusinessObject/Models/Publisher.cs
@@ -7,11 +7,13 @@ public partial class Publisher
 {
     public int PublisherId { get; set; }
 
-    public string? PublisherName { get; set; }
+    public string PublisherName { get; set; } = null!;
 
     public string? PublisherEmail { get; set; }
 
     public string? PublisherDescription { get; set; }
 
     public string? PublisherPhoneNo { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 }
