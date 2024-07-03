b19ee7686f1c9854c51f82255ff7528ab12c45ba
PhucHong-6113 <phuongdubu@gmail.com>
2024-07-03 15:48:51 +0700

    update database and docs

---
+++ BookStoreManagement/MenuScreen.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace PresentationObject
+{
+    partial class MenuScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(130, 180);
+            button1.Name = "button1";
+            button1.Size = new Size(132, 100);
+            button1.TabIndex = 0;
+            button1.Text = "Book Management";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(365, 338);
+            button2.Name = "button2";
+            button2.Size = new Size(130, 91);
+            button2.TabIndex = 1;
+            button2.Text = "Category Management";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(130, 338);
+            button3.Name = "button3";
+            button3.Size = new Size(132, 91);
+            button3.TabIndex = 2;
+            button3.Text = "Publisher Management";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(365, 180);
+            button4.Name = "button4";
+            button4.Size = new Size(130, 100);
+            button4.TabIndex = 3;
+            button4.Text = "Author Management";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 53);
+            label1.Name = "label1";
+            label1.Size = new Size(116, 65);
+            label1.TabIndex = 4;
+            label1.Text = "Huh";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(201, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(441, 65);
+            label2.TabIndex = 5;
+            label2.Text = "I am feeling lucking";
+            // 
+            // MenuScreen
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(642, 480);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "MenuScreen";
+            Text = "MainStoreScreen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+    }
+}
