0a47e4b43955650b8d3dc8efb3b6ea970ac939a6
PhucHong-6113 <phuongdubu@gmail.com>
2024-07-07 22:59:16 +0700

    double click cell

--- BookStoreManagement/Books/BookList.cs
+++ BookStoreManagement/Books/BookList.cs
@@ -1,4 +1,5 @@
-﻿using DataAccessObject;
+﻿using BusinessObject.Models;
+using DataAccessObject;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -30,15 +31,13 @@ namespace PresentationObject.Books
                 dgv_books.DataSource = null;
                 dgv_books.DataSource = _bookRepository.GetBooks(a => a != null);
             }
-
-
         }
 
         private void refreshData()
         {
             dgv_books.DataSource = null;
             dgv_books.DataSource = _bookRepository.GetBooks
-                (b => b.BookName.Contains(search_txt.Text))
+                (b => b.BookName.Contains(search_txt.Text) || b.Author.AuthorName.Contains(search_txt.Text) || b.Category.CategoryName.Contains(search_txt.Text) || b.Publisher.PublisherName.Contains(search_txt.Text))
                 .Select(b => new
                 {
                     b.BookId,
@@ -49,7 +48,17 @@ namespace PresentationObject.Books
                     b.Quantity,
                     b.Price,
                 }).ToList();
-            book_count_label.Text = (_bookRepository.CountBooks() + " books existed!!");
+            if (search_txt.Text == "")
+            {
+                book_count_label.Text = (_bookRepository.CountBooks() + " books existed!!");
+            }
+            else
+            {
+                var dataSource = dgv_books.DataSource as IList<object>;
+                int count = dataSource?.Count ?? 0;
+                book_count_label.Text = (count + " book(s) match your search!!");
+            }
+
         }
 
         private void book_detail_Click(object sender, EventArgs e)
@@ -72,6 +81,38 @@ namespace PresentationObject.Books
         private void search_btn_Click(object sender, EventArgs e)
         {
             refreshData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MenuScreen _menuScreen = new MenuScreen();
+            //_menuScreen.Show();
+            //this.Close();
+        }
+
+        private void enter(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                refreshData();
+
+            }
+        }
+
+        private void List_doubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                // Get the BookId of the double-clicked row
+                int bookId = Convert.ToInt32(dgv_books.Rows[e.RowIndex].Cells["BookId"].Value);
+
+                // Retrieve the selected book details
+                Book selectedBook = _bookRepository.GetBook(bookId);
+
+                BookDetail bookDetail = new BookDetail(selectedBook.BookId, this);
+                bookDetail.Show();
+                this.Hide();
+            }
         }
     }
 }
